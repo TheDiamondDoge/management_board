e4258c0a529a00272af44b0c902d18357b1a806c
TheDiamondDoge <supportonly@yandex.ru>
2020-03-03 16:48:45 +0300

    summary tab service refactor

--- projects_api/src/main/java/com/aiksanov/api/project/business/service/SummaryTabService.java
+++ projects_api/src/main/java/com/aiksanov/api/project/business/service/SummaryTabService.java
@@ -6,6 +6,7 @@ import com.aiksanov.api.project.data.entity.StatusReport;
 import com.aiksanov.api.project.data.repository.GeneralRepository;
 import com.aiksanov.api.project.data.repository.ProjectURLsRepository;
 import com.aiksanov.api.project.data.repository.StatusReportRepository;
+import com.aiksanov.api.project.exceptions.ProjectDoesNotExist;
 import com.aiksanov.api.project.web.DTO.summary.SummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,8 +31,7 @@ public class SummaryTabService {
     }
 
     public SummaryDTO getSummaryDTO(Integer projectID){
-        Project projectInfo = this.generalRepo.findById(projectID)
-                .orElseThrow(() -> new RuntimeException("Project id doesn't exist"));
+        Project projectInfo = this.generalRepo.findById(projectID).orElseThrow(ProjectDoesNotExist::new);
         ProjectURLs urls = this.urlsRepo.findById(projectID).orElseGet(ProjectURLs::new);
         StatusReport report = this.reportRepo.findById(projectID).orElseGet(StatusReport::new);
         int activeActionsAmount = this.actionsService.getActiveActions(projectID);
