4c98f0b4ac2a99c9fe3698e554055c8e7feabda0
TheDiamondDoge <supportonly@yandex.ru>
2020-06-14 23:51:58 +0300

    editing h2 db schema

--- projects_api/src/main/resources/schema.sql
+++ projects_api/src/main/resources/schema.sql
@@ -26,7 +26,6 @@ CREATE TABLE IF NOT EXISTS `prj_products` (
   `product_release` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`project_id`)
 );
-ALTER TABLE `prj_products` ADD FOREIGN KEY (project_id) REFERENCES `PRJ_WORKSPACE_GENERAL`(project_id);
 
 DROP TABLE IF EXISTS prj_milestones;
 CREATE TABLE IF NOT EXISTS `prj_milestones` (
@@ -39,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `prj_milestones` (
   `show_in_timeline` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`project_id`,`milestone_label`)
 );
-ALTER TABLE `prj_milestones` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_milestones` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_workspace_info;
 CREATE TABLE IF NOT EXISTS `prj_workspace_info` (
@@ -50,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `prj_workspace_info` (
   `workspace_modified_by` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`project_id`)
 );
-ALTER TABLE `prj_workspace_info` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_workspace_info` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_indicators_health;
 CREATE TABLE IF NOT EXISTS `prj_indicators_health` (
@@ -63,13 +62,13 @@ CREATE TABLE IF NOT EXISTS `prj_indicators_health` (
   `modification_date` date NOT NULL,
   PRIMARY KEY (`project_id`, `modification_date`)
 );
-ALTER TABLE `prj_indicators_health` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_indicators_health` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_indicators_comments;
 CREATE TABLE IF NOT EXISTS `prj_indicators_comments` (
   `project_id` int(5) NOT NULL,
   `label` varchar(10) DEFAULT 0,
-  `comment` text DEFAULT 0,
+  `comment` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`project_id`, `label`)
 );
 -- ALTER TABLE `prj_indicators_comments` ADD FOREIGN KEY (project_id) REFERENCES prj_indicators_health(project_id);
@@ -86,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `prj_additional_info` (
   `maintenance` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`project_id`)
 );
-ALTER TABLE `prj_additional_info` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_additional_info` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_jira_params;
 CREATE TABLE IF NOT EXISTS `prj_jira_params` (
@@ -95,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `prj_jira_params` (
     `rq_release` varchar(100) DEFAULT NULL,
     PRIMARY KEY (`project_id`)
 );
-ALTER TABLE `prj_jira_params` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_jira_params` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_urls;
 CREATE TABLE IF NOT EXISTS `prj_urls` (
@@ -116,15 +115,15 @@ CREATE TABLE IF NOT EXISTS `prj_urls` (
   `cis_url` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`project_id`)
 );
-ALTER TABLE `prj_urls` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_urls` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_field_comments;
 CREATE TABLE IF NOT EXISTS `prj_field_comments` (
     `project_id` int(5) NOT NULL,
     `field_name` VARCHAR(25) NOT NULL,
-    `comment` VARCHAR(255) DEFAULT NULL
+    `comment` VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (`project_id`,`field_name`)
 );
-ALTER TABLE `prj_field_comments` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_ecma_backlog_target;
 CREATE TABLE IF NOT EXISTS `prj_ecma_backlog_target` (
@@ -133,7 +132,7 @@ CREATE TABLE IF NOT EXISTS `prj_ecma_backlog_target` (
   `value` int(5) DEFAULT 0,
   PRIMARY KEY (`project_id`, `milestone_label`)
 );
-ALTER TABLE `prj_ecma_backlog_target` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_ecma_backlog_target` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_contrib;
 CREATE TABLE IF NOT EXISTS `prj_contrib` (
@@ -141,7 +140,6 @@ CREATE TABLE IF NOT EXISTS `prj_contrib` (
     `contrib_id` int(5) NOT NULL,
     PRIMARY KEY (`project_id`, `contrib_id`)
 );
-ALTER TABLE `prj_contrib` ADD FOREIGN KEY (`project_id`) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_status_report;
 CREATE TABLE IF NOT EXISTS `prj_status_report` (
@@ -153,27 +151,33 @@ CREATE TABLE IF NOT EXISTS `prj_status_report` (
     `details` text,
     PRIMARY KEY (`project_id`)
 );
-ALTER TABLE `prj_status_report` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_status_report` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_status_report_snapshots;
-CREATE TABLE IF NOT EXISTS `prj_status_report_snapshots` (
-    `project_id` int(5) NOT NULL,
-    `report_id` int(3) NOT NULL,
-    `timestamp` timestamp NOT NULL,
-    `snapshot_json` text,
-    PRIMARY KEY (`project_id`, `report_id`)
+CREATE TABLE `prj_status_report_snapshots` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ppt_snapshot` mediumblob,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `prj_status_report_snapshots_info` (
+  `id` int(11) NOT NULL,
+  `project_id` int(5) DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 DROP TABLE IF EXISTS prj_indicators_reqs;
 CREATE TABLE IF NOT EXISTS `prj_indicators_reqs` (
     `project_id` int(5) NOT NULL,
-    `committed_at_dr1` int(6),
-    `added_after_dr1` int(6),
-    `removed_after_dr1` int(6),
-    `modified_after_dr1` int(6),
+    `committed_at_dr1` int(6) DEFAULT NULL,
+    `added_after_dr1` int(6) DEFAULT NULL,
+    `removed_after_dr1` int(6) DEFAULT NULL,
+    `modified_after_dr1` int(6) DEFAULT NULL,
     PRIMARY KEY (`project_id`)
 );
-ALTER TABLE `prj_indicators_reqs` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
+-- ALTER TABLE `prj_indicators_reqs` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_blc_dashboard;
 CREATE TABLE IF NOT EXISTS `prj_blc_dashboard` (
@@ -195,16 +199,14 @@ CREATE TABLE IF NOT EXISTS `prj_blc_dashboard` (
     `risks` int(1) DEFAULT 0,
     PRIMARY KEY (`project_id`, `role`)
 );
-ALTER TABLE `prj_blc_dashboard` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_blc_dashboard_comments;
 CREATE TABLE IF NOT EXISTS `prj_blc_dashboard_comments` (
     `project_id` int(5) NOT NULL,
     `role` varchar(5) NOT NULL,
-    `comment` text,
+    `comment` varchar(255),
     PRIMARY KEY (`project_id`, `role`)
 );
-ALTER TABLE `prj_blc_dashboard_comments` ADD FOREIGN KEY (project_id, role) REFERENCES prj_blc_dashboard(project_id, role);
 
 DROP TABLE IF EXISTS prj_indicators_quality;
 CREATE TABLE IF NOT EXISTS `prj_indicators_quality` (
@@ -215,16 +217,15 @@ CREATE TABLE IF NOT EXISTS `prj_indicators_quality` (
     `actual` varchar(512),
     PRIMARY KEY (`project_id`, `kpi_id`, `row_num`)
 );
-ALTER TABLE `prj_indicators_quality` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_indicators_quality_comments;
 CREATE TABLE IF NOT EXISTS `prj_indicators_quality_comments` (
     `project_id` int(5) NOT NULL,
     `kpi_id` varchar(15) NOT NULL,
-    `comment` text,
+    `comment` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`project_id`, `kpi_id`)
 );
--- ALTER TABLE `prj_indicators_quality_comments` ADD FOREIGN KEY (project_id, kpi_id) REFERENCES prj_indicators_quality(project_id, kpi_id);
+-- -- ALTER TABLE `prj_indicators_quality_comments` ADD FOREIGN KEY (project_id, kpi_id) REFERENCES prj_indicators_quality(project_id, kpi_id);
 
 DROP TABLE IF EXISTS prj_risks;
 CREATE TABLE IF NOT EXISTS `prj_risks` (
@@ -249,63 +250,45 @@ CREATE TABLE IF NOT EXISTS `prj_risks` (
     `target` date,
     `done` date,
     `result` date,
+    `updated_on` timestamp NULL DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL,
     `report` tinyint(1),
     PRIMARY KEY (`project_id`, `risk_id`)
 );
-ALTER TABLE `prj_risks` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
-DROP TABLE IF EXISTS prj_actions_registry;
-CREATE TABLE IF NOT EXISTS `prj_actions_registry` (
-    `registry_id` int(1) NOT NULL,
-    `registry_label` varchar(20),
-    PRIMARY KEY (registry_id)
+DROP TABLE IF EXISTS prj_risks_info;
+CREATE TABLE `prj_risks_info` (
+  `project_id` int(5) NOT NULL,
+  `uploaded_on` timestamp NULL DEFAULT NULL,
+  `uploaded_by` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`project_id`)
 );
 
-DROP TABLE IF EXISTS prj_actions_state;
-CREATE TABLE IF NOT EXISTS `prj_actions_state` (
-    `state_id` int(1) NOT NULL,
-    `state_label` varchar(20),
-    PRIMARY KEY (state_id)
-);
-
-DROP TABLE IF EXISTS prj_actions_priority;
-CREATE TABLE IF NOT EXISTS `prj_actions_priority` (
-    `priority_id` int(1) NOT NULL,
-    `priority_label` varchar(20),
-    PRIMARY KEY (priority_id)
-);
 
 DROP TABLE IF EXISTS prj_actions;
 CREATE TABLE IF NOT EXISTS `prj_actions` (
     `project_id` int(5) NOT NULL,
-    `registry` int(1),
-    `uid` int(5) AUTO_INCREMENT NOT NULL,
-    `title` varchar(255),
-    `state` int(1),
-    `priority` int(1),
-    `owner` varchar(255),
+    `registry` varchar(20) DEFAULT NULL,
+    `uid` int(5) NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) DEFAULT NULL,
+    `state` varchar(20) DEFAULT NULL,
+    `priority` varchar(20) DEFAULT NULL,
+    `owner` varchar(255) DEFAULT NULL,
     `optional_info` text,
-    `due` date,
+    `due` date DEFAULT NULL,
     `description` text,
     `status` text,
-    `created_date` timestamp,
-    `closed_date` timestamp,
-    foreign key (`state`) references prj_actions_state(`state_id`),
-    foreign key (`registry`) references prj_actions_registry(`registry_id`),
-    foreign key (`priority`) references prj_actions_priority(`priority_id`),
+    `created_date` timestamp NULL DEFAULT NULL,
+    `closed_date` timestamp NULL DEFAULT NULL,
     PRIMARY KEY (`uid`)
 );
-ALTER TABLE `prj_actions` ADD FOREIGN KEY (project_id) REFERENCES PRJ_WORKSPACE_GENERAL(project_id);
 
 DROP TABLE IF EXISTS prj_actions_related_risks;
 CREATE TABLE IF NOT EXISTS `prj_actions_related_risks` (
     `action_id` int(3) NOT NULL,
-    `risk_id` int(3) NOT NULL,
+    `risk_id` varchar(5) NOT NULL,
     `project_id` int(5) NOT NULL,
     PRIMARY KEY (action_id, risk_id, project_id)
---    FOREIGN KEY (action_id) REFERENCES  `prj_actions` (uid),
---    FOREIGN KEY (risk_id) REFERENCES  `prj_risks` (risk_id),
---    FOREIGN KEY (project_id) REFERENCES  `prj_risks` (project_id)
 );
 
 DROP TABLE IF EXISTS prj_cost;
@@ -313,8 +296,8 @@ CREATE TABLE IF NOT EXISTS `prj_cost` (
     `project_id` int(5) NOT NULL,
     `type` int(1) NOT NULL,
     `state` int(1) NOT NULL,
-    `milestone` varchar(10),
-    `value` double,
+    `milestone` varchar(10) DEFAULT NULL,
+    `value` double DEFAULT NULL,
     `comment` text,
     PRIMARY KEY (`project_id`, `type`, `state`)
 );
